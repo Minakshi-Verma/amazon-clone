cde8c8f7a4f017f15af3563667609f0573cd142e
Minakshi Verma <mvedwan@gmail.com>
2020-09-13 17:09:18 -0400

    product schema is defined!

--- amazonia/backend/models/productModel.js
+++ amazonia/backend/models/productModel.js
@@ -1,13 +1,22 @@
 import mongoose from 'mongoose';
 
 //Schema for the user model
-const prodSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
     name:{type:String, required: true},
-    email:{type:String, required: true, unique: true, dropDups:true},
-    password:{type:String, required: true},
-    isAdmin:{type: Boolean, required: true, default: false}
+    image:{type:String, required: true},
+    brand:{type:String, required: true},
+    Price:{type:Number, default:0, required: true},
+    category:{type:String, required: true},
+    countinStock:{type:Number,default:0, required: true},
+    description:{type:String, required: true},
+    rating:{type:Number, required: true},
+    numReviews:{type:Number, default:0, required: true},
+    
+
+   
 })
 
-const userModel = mongoose.model("User", userSchema)
+// In database productSchema will be saved with the name: Product
+const productModel = mongoose.model("Product", productSchema)
 
-export default userModel;
+export default productModel;
