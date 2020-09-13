ee042ee81859ae76a8509372b23775098fa1e811
Minakshi Verma <mvedwan@gmail.com>
2020-09-13 16:59:18 -0400

    user authentication and admin authentication added

--- amazonia/backend/util.js
+++ amazonia/backend/util.js
@@ -13,4 +13,32 @@ const getToken = (user) =>{
     })
 }
 
-export default getToken
+//user authentication added using express middleware
+ const isAuth = (req, res, next)=>{
+    const token = req.headers.authorization;
+    if(token){
+        const onlyToken = token.slice(7, token.length);
+        jwt.verify(onlyToken, config.JWT_SECRET, (err, decode)=>{
+            if(err){
+                return res.status(401).send({msg: "invalid Token"})
+            }
+            req.user = token
+            next();
+            return
+        })
+    }
+    return res.status(401).send({msg: "Token is not supplied"})
+ }
+
+ //admin authentication added using express middleware
+ const isAdmin = (req,res,next) =>{
+     //check is req.user exist and isAdmin is true to verify
+    if(req.user && req.user.isAdmin){
+        return next()
+    }
+    return res.status(401).send({msg: "Admin token is not valid"})
+ }
+
+export  {
+    getToken, isAuth, isAdmin
+}
